a2c1d328142f385931af44500c158789c8e824d2
Syed Junayed Ahmed Anta <57961109+junayed786@users.noreply.github.com>
2020-11-02 14:56:12 +0100

    UTCI_Range

--- R/validator.R
+++ R/validator.R
@@ -1,24 +1,7 @@
+load("~/Documents/GitHub/comf/data/dfField.RData")
+load("~/Documents/GitHub/comf/data/dfASHRAETableG11.RData")
+load("~/Documents/GitHub/comf/data/dfISO7730TableD1.RData")
 #validation for UTCI
-#' @title Validation for Calculating UTCI
-#' @description Functions to validate UTCI .
-#' @usage call calcUTCI(ta, tr, vel, rh), validate_UTCI will be called automatically
-#' @param ta a numeric value presenting air temperature in [degree C]
-#' @param tr a numeric value presenting mean radiant temperature in [degree C]
-#' @param vel a numeric value presenting air velocity in [m/s]
-#' @param rh a numeric value presenting relative humidity [\%]
-#' @details inputs have to be numerical and not null. Also, the values have to be of certain range.
-#' @return Gives a warning message if any of the inputs are not numerical, null and out of rage. If the inputs are valid then nothing is done.
-#' @author Code implemented in to R by Shaomi Rahman
-#' @export
-#' @examples
-#' validate_UTCI(25, 25, 0.1, 50) #valid input
-#' does nothing
-#' validate_UTCI(FALSE, 50, 0.1, 50) #not numerical ta input
-#' validate_UTCI(50, 50, 0.1, 50) #invalid input for ta, td
-#' Warning Message: 50 is out of range. Has to be between 0 and 40"
-#'                  50 is out of range. Has to be between 0 and 40
-#' validate_UTCI(25, 25, 0.1, 150) #invalid input for rh
-#' Warning Message: 150 is out of range. Has to be between 0 and 100"
 validate_UTCI <- function(ta, tr, vel, rh) {
  parameters <- c(ta, tr, vel, rh)
  
@@ -38,10 +21,29 @@ validate_UTCI <- function(ta, tr, vel, rh) {
  
  #check range for rh
  check_range(rh, 0, 100)
+ 
+ #check_range for UTCI
+ min = get_utci_range()[1]
+ max = get_utci_range()[2]
+ check_range(UTCI, min, max)
 }
 
 #check range for parameters
 check_range <- function(parameter, lower_bound, upper_bound) {
   if( parameter < lower_bound || parameter > upper_bound)
     warning(paste(parameter, " is out of range. Has to be between ",lower_bound, " and ",upper_bound))
+}
+
+get_utci_range <- function() {
+  samples = Reduce(function(x, y) merge(x, y, all=TRUE), list(dfField, dfASHRAETableG11,dfISO7730TableD1))
+  UTCI_Value = data.frame(ta=numeric(0),tr=numeric(0),vel=numeric(0),rh=numeric(0),UTCI_value=numeric(0))
+  for(i in 1:nrow(samples)) {
+    row <- samples[i,]
+    ta <- row$ta
+    tr <- row$tr
+    vel <- row$vel
+    rh <- row$rh
+    UTCI_Value[nrow(UTCI_Value) + 1,] = list(ta,tr,vel,rh,calcUTCI(ta,tr,vel,rh))
+  }
+  c( min(UTCI_Value$UTCI_value), max(UTCI_Value$UTCI_value) )
 }
