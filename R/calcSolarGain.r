1fe4b7f407e1efda1a89eab19de12cbd3b2b4cb7
Syed Junayed Ahmed Anta <57961109+junayed786@users.noreply.github.com>
2020-12-05 14:14:15 +0100

    implementation of UTCI and Solar Gain

---
+++ R/calcSolarGain.r
@@ -0,0 +1,146 @@
+#' Calculating Solar Gain
+#'
+#' @usage calcSolarGain(solAlt, solAzi, solRadDir, solTrans,
+#'  fSvv, fBes, asw=0.7, posture="seated", floorRef=0.6)
+#' 
+#' @param solAlt a numeric value presenting Solar altitude, degrees from horizontal in [degree C]
+#' @param solAzi a numeric value presenting Solar azimuth, degrees clockwise from North in [degree C]
+#' @param solRadDir a numeric value presenting Direct-beam solar radiation [W/m2]
+#' @param solTrans a numeric value presenting Total solar transmittance. Ranges from 0 to 1
+#' @param fSvv a numeric value presenting fraction of sky vault exposed to body, ranges from 0 to 1
+#' @param fBes a numeric value presenting fraction of the possible body surface exposed to sun, ranges from 0 to 1
+#' @param asw a numeric value presenting the average short-wave absorptivity of the occupant. Set to 0.7 by default
+#' @param posture Default 'seated' list of available options 'standing', 'supine' or 'seated'
+#' @param floorRef a numeric value presenting floor refectance. It is assumed to be constant and equal to 0.6.
+#'
+#' @return An array of 2 values
+#' First values represents \code{erf} - Solar gain to the human body using the Effective Radiant Field [W/m2]
+#' Second value represents \code{delMrt} - Delta mean radiant temperature [Degree C]
+#' 
+#' @description Functions to calculate Solar Gain.
+#' @details Inputs are - 
+#' Solar altitude, Solar azimuth should be in Degree C unit. 
+#' Direct-beam solar radiation has to be in W/m2.
+#' Solar transmittance, Fraction of sky vault, fraction of the possible body surface, fraction of the possible body surface, average short-wave absorptivity of the occupant are decimal values. Has to be from 0 to 1. 
+#' Posture is a string input. Has be of one followings - 'standing', 'supine' or 'seated'. 
+#' The outputs are - 
+#' Solar gain to the human body using the Effective Radiant Field. The unit is W/m2
+#' Delta mean radiant temperature in Degree C
+#' @export
+#' @aliases calcSolarGain calcsolargain SolarGain solargain Solar  Gain solar  gain
+#' @examples
+#' calcSolarGain(0, 120, 800, 0.5, 0.5, 0.5, asw=0.7, posture="seated") = [42.9, 10.3]
+#' @author Code implemented in to R by Shaomi Rahman. Further contribution by Marcel Schweiker.
+#' @seealso see also calcComfInd
+calcSolarGain <- function(solAlt, solAzi, solRadDir, solTrans, 
+                          fSvv, fBes, asw=0.7, 
+                          posture="seated", floorRef=0.6){
+  
+  posture = tolower(posture)
+  solarGainRes = solarGain(solAlt, solAzi, solRadDir, solTrans, fSvv, fBes, asw, 
+                           posture, floorRef=0.6)
+  if(!exists("solarGainRange")){
+    solarGainRange <<- getSolarGainRange()
+  }
+  check_range(solarGainRes[1], solarGainRange[1],solarGainRange[2])
+  check_range(solarGainRes[2], solarGainRange[3],solarGainRange[4])
+  solarGainRes
+}
+
+solarGain <- function(solAlt, solAzi, solRadDir, solTrans, 
+                      fSvv, fBes, asw=0.7, 
+                      posture="seated", floorRef=0.6){
+  
+  deg_to_rad = 0.0174532925
+  hr = 6
+  i_diff = 0.2 * solRadDir
+  
+  fp_table = rbind(
+    c(0.25, 0.25, 0.23, 0.19, 0.15, 0.10, 0.06),
+    c(0.25, 0.25, 0.23, 0.18, 0.15, 0.10, 0.06),
+    c(0.24, 0.24, 0.22, 0.18, 0.14, 0.10, 0.06),
+    c(0.22, 0.22, 0.20, 0.17, 0.13, 0.09, 0.06),
+    c(0.21, 0.21, 0.18, 0.15, 0.12, 0.08, 0.06),
+    c(0.18, 0.18, 0.17, 0.14, 0.11, 0.08, 0.06),
+    c(0.17, 0.17, 0.16, 0.13, 0.11, 0.08, 0.06),
+    c(0.18, 0.18, 0.16, 0.13, 0.11, 0.08, 0.06),
+    c(0.20, 0.20, 0.18, 0.15, 0.12, 0.08, 0.06),
+    c(0.22, 0.22, 0.20, 0.16, 0.13, 0.09, 0.06),
+    c(0.24, 0.24, 0.21, 0.17, 0.13, 0.09, 0.06),
+    c(0.25, 0.25, 0.22, 0.18, 0.14, 0.09, 0.06),
+    c(0.25, 0.25, 0.22, 0.18, 0.14, 0.09, 0.06)
+  )
+  
+  if(posture == "seated"){
+    fp_table = rbind(
+      c(0.20, 0.23, 0.21, 0.21, 0.18, 0.16, 0.12),
+      c(0.20, 0.23, 0.20, 0.20, 0.19, 0.16, 0.12),
+      c(0.20, 0.23, 0.21, 0.20, 0.18, 0.15, 0.12),
+      c(0.19, 0.23, 0.20, 0.20, 0.18, 0.15, 0.12),
+      c(0.18, 0.21, 0.19, 0.19, 0.17, 0.14, 0.12),
+      c(0.16, 0.20, 0.18, 0.18, 0.16, 0.13, 0.12),
+      c(0.15, 0.18, 0.17, 0.17, 0.15, 0.13, 0.12),
+      c(0.16, 0.18, 0.16, 0.16, 0.14, 0.13, 0.12),
+      c(0.18, 0.18, 0.16, 0.14, 0.14, 0.12, 0.12),
+      c(0.19, 0.18, 0.15, 0.13, 0.13, 0.12, 0.12),
+      c(0.21, 0.18, 0.14, 0.12, 0.12, 0.12, 0.12),
+      c(0.21, 0.17, 0.13, 0.11, 0.11, 0.12, 0.12),
+      c(0.21, 0.17, 0.12, 0.11, 0.11, 0.11, 0.12)
+    )
+  }
+  
+  if(posture == "supine"){
+    alt_temp = solAlt
+    solAlt = abs(90 - solAzi)
+    solAzi = alt_temp
+  }
+  
+  alt_range = c(0, 15, 30, 45, 60, 75, 90)
+  az_range = c(0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180)
+  alt_i = find_span(alt_range, solAlt)
+  az_i = find_span(az_range, solAzi)
+  fp11 = fp_table[az_i,alt_i]
+  fp12 = fp_table[az_i,alt_i + 1]
+  fp21 = fp_table[az_i + 1,alt_i]
+  fp22 = fp_table[az_i + 1,alt_i + 1]
+  az1 = az_range[az_i]
+  az2 = az_range[az_i + 1]
+  alt1 = alt_range[alt_i]
+  alt2 = alt_range[alt_i + 1]
+  fp = fp11 * (az2 - solAzi) * (alt2 - solAlt)
+  fp = fp + (fp21 * (solAzi - az1) * (alt2 - solAlt))
+  fp = fp +(fp12 * (az2 - solAzi) * (solAlt - alt1))
+  fp = fp +(fp22 * (solAzi - az1) * (solAlt - alt1))
+  fp = fp/((az2 - az1) * (alt2 - alt1))
+  f_eff = 0.725
+  
+  if(posture == "seated")
+    f_eff = 0.696
+  
+  sw_abs = asw
+  lw_abs = 0.95
+  
+  e_diff = f_eff * fSvv * 0.5 * solTrans * i_diff
+  e_direct = fp * solTrans * fBes * solRadDir
+  e_refl = (
+    f_eff 
+    * fSvv 
+    * 0.5 
+    * solTrans 
+    * (solRadDir * sin(solAlt * deg_to_rad) + i_diff) 
+    * floorRef
+  )
+  e_solar = e_diff + e_direct + e_refl
+  erf = e_solar * (sw_abs / lw_abs)
+  delMrt = erf / (hr * f_eff)
+  return(c(round(erf,1),round(delMrt,1)))
+}
+
+find_span <- function(arr, x){
+  for(i in seq(1, length(arr)-1)){
+    if (arr[i] <= x && arr[i+1] >= x){
+      return(i)
+    }
+  }
+  return(-1)
+}
---
+++ R/validator.r
@@ -0,0 +1,103 @@
+#validation for UTCI input
+validate_UTCI <- function(ta, tr, vel, rh) {
+  parameters <- c(ta, tr, vel, rh)
+  
+  #check if parameter is numeric and not null
+  for (parameter in parameters) {
+    if(!is.numeric(parameter) || is.null(parameter))
+      warning(paste(parameter," is not valid (has to be numeric and not null)"))
+  }
+  
+  #check range for ta & tr
+  for (parameter in c(ta, tr)) {
+    check_range(parameter, 0, 40)
+  }
+  
+  #check range for vel
+  check_range(vel, 0, 10)
+  
+  #check range for rh
+  check_range(rh, 0, 100)
+}
+
+#validation for Solar Gain Input
+validateSolarGain <- function(solAlt, solAzi, solRadDir, solTrans, fSvv, 
+                              fBes, asw=0.7, posture="seated", floorRef=0.6) {
+  
+  #check if parameters are  not null
+  for (parameter in c(solAlt, solAzi, solRadDir, solTrans, fSvv, fBes, asw, 
+                      posture, floorRef)) {
+    if(is.null(parameter))
+      warning(paste(parameter," is not valid (cannot be null)"))
+  }
+  
+  #check if parameters are numeric
+  for (parameter in c(solAlt, solAzi, solRadDir, solTrans, fSvv, fBes, asw, floorRef)) {
+    if(!is.numeric(parameter))
+      warning(paste(parameter," is not valid (has to be numeric)"))
+  }
+  
+  #validate posture input
+  if(!posture %in% c("standing", "supine", "seated"))
+    stop("Posture has to be either standing, supine or seated")
+  
+  #check range for Solar altitude
+  check_range(solAlt, 0, 90)
+  
+  #check range for Solar azimuth
+  check_range(solAzi, 0, 180)
+  
+  #check range for Direct-beam solar radiation
+  check_range(solRadDir, 200, 1000)
+  
+  #check ranges for solTrans, fSvv, fBes, asw
+  for (parameter in c(solTrans, fSvv, fBes, asw)) {
+    check_range(parameter, 0, 1)
+  }
+}
+
+#check range for parameters
+check_range <- function(parameter, lower_bound, upper_bound) {
+  if( parameter < lower_bound || parameter > upper_bound)
+    stop(paste(parameter, " is out of range. Has to be between ",lower_bound, " and ",upper_bound))
+}
+
+#get acceptable utci range
+getUtciRange <- function() {
+  ta = tr = c(0,20,40)
+  vel = c(0,5,10)
+  rh = c(0,50,100)
+  utciValue = expand.grid(ta = ta, tr = tr, vel = vel, rh = rh)
+  utci = c()
+  for(i in 1:nrow(utciValue)){
+    row <- utciValue[i,]
+    utci = append(utci, utci_approx(row$ta, row$tr, row$vel, row$rh))
+  }
+  utciValue$UTCI = utci
+  save(utciValue, file = "/Users/junayed/Documents/GitHub/comf/data/utciValue.RData")
+  return(c(min(utciValue$UTCI), max(utciValue$UTCI)))
+}
+
+getSolarGainRange <- function() {
+  solAlt = c(0, 90) 
+  solAzi = c(0, 180) 
+  solRadDir = c(200, 2000) 
+  solTrans = fSvv = fBes = asw = c(0,1)
+  posture= c("standing", "supine", "seated")
+  solarGainValue = expand.grid(solAlt = solAlt, solAzi = solAzi, 
+                               solRadDir = solRadDir, solTrans = solTrans,
+                               fSvv = fSvv, fBes = fBes, asw = asw,
+                               posture = posture, erf = 0, dMart = 0)
+  for(i in 1:nrow(solarGainValue)){
+    solarGainRes = solarGain(solarGainValue$solAlt[i], solarGainValue$solAzi[i],
+                             solarGainValue$solRadDir[i], 
+                             solarGainValue$solTrans[i], solarGainValue$fSvv[i],
+                             solarGainValue$fBes[i], solarGainValue$asw[i],
+                             solarGainValue$posture[i])
+    solarGainValue$erf[i] = solarGainRes[1]
+    solarGainValue$dMart[i] = solarGainRes[2]
+  }
+  #save(solarGainValue, file = "/Users/junayed/Documents/GitHub/comf/data/solarGainValue.Rdata")
+  c(min(solarGainValue$erf), max(solarGainValue$erf), 
+    min(solarGainValue$dMart), max(solarGainValue$dMart))
+}
