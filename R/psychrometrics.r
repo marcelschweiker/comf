ad134837e1f762edb1df6ad7e863998ef24e8595
Shaomi Rahman <shaomi.rahman@rwth-aachen.de>
2021-02-19 12:17:31 +0100

    Initial commit

--- R/psychrometrics.r
+++ R/psychrometrics.r
@@ -1,181 +1,188 @@
-cToK = 273.15
-cpVapour = 1805.0
-cpWater = 4186
-cpAir = 1004
-hFg = 2501000
-rAir = 287.055
+library(ggplot2)
+library(dplyr)
 
-#Estimates the saturation vapor pressure in [torr]
-pSatTorr <- function(tdb){
-  exp(18.6686 - 4030.183 / (tdb + 235.0))
-}
+#air temperature range over which calculations are performed [oC]
+tempAirInit = seq(from = 10, to = 34.5, by = 0.5)
 
-#Estimates the relative air velocity which combines the average air velocity of 
-#the space plus the relative air velocity caused by the body movement.
-vRelative <- function(vel, met){
-  vr = ifelse(met > 1, round(vel + 0.3 * (met - 1), 3), vel)
-  vr
-}
+#%RH range over which calculations are performed [%RH/100]
+RHPsyInit = seq(from = 0, to = 0.99, by = 0.01)
 
-#Estimates the dynamic clothing insulation of a moving occupant. The activity as
-#well as the air speed modify the insulation characteristics of the clothing and 
-#the adjacent air layer. Consequently the ISO 7730 states that the clothing insulation
-#shall be corrected [2]_. The ASHRAE 55 Standard, instead, only corrects for the effect
-#of the body movement, and states that the correction is permitted but not required.
-cloDynamic <- function(clo, met, standard = "ASHARE"){
-  if(!tolower(standard) %in% c("ashare")){
-    stop("PMV calculations can only be performed in compliance with ISO or ASHRAE Standards")
-  }
+#metabolic rate for a person in [W/m2]. default value is 69.8 W/m2, equivalent to 1.2 met (the other unit for metabolic rate)
+MROrig = 1.2
+
+#do not change, changes MR_orig inputs in 'met' to W/m2
+MR= MROrig*58.2
+
+#skin wettedness parameter, [unitless]
+#0.06 for dry skin, accounts for diffusion through skin
+#0.8 is the practical upper bound for very sweaty skin
+w=0.06
+
+#air speed in [m/s]
+v=0.3
+
+#the air temperature minus the mean radiant temperature (MRT) [K]
+#this is specified to calculate a wind speed for thermal comfort
+#values > 0 are for an air temperature greater than the MRT
+#values < 0 are for an air temperature lower than the MRT
+dep = 0.0000
+
+#emissivity of human skin [unitless]
+#values in the literature vary between 0.95 to 0.98
+E=0.95
+
+#Stephan Botlztmans constant
+o = 0.00000005670367
+
+#empirical ratio for a seated person
+ArAd = 0.7
+
+#empirical ratio for calculating the evaporative "heat transfer" coefficient
+LR = 16.5
+
+#define psychrometric temp bounds for drawing %RH lines
+temperature = seq(from = 5, to = 39.9, by = 0.1)
+
+#redefine temp and %RH arrays
+tempAir = matrix(array(), nrow=50, ncol = 100)
+RHPsy = matrix(array(), nrow=50, ncol=100)
+
+#skin temp calculation
+tempSkin = matrix(array(), nrow=50, ncol=100)
+
+#vapor pressure of water on skin's surface
+PSatSkinPsy =  matrix(array(), nrow=50, ncol=100)
+
+#mapping %RH matrix to specific humidity for plotting
+psySat = matrix(array(), nrow=50, ncol=100)
+
+#vapor pressure of water in air
+PSatAirPsy = matrix(array(), nrow=50, ncol=100)
+
+#free convection heat transfer coefficient for seated person
+hCFreePsy = matrix(array(), nrow=50, ncol=100)
+
+#free convection around the human body
+QConvFreePsy = matrix(array(), nrow=50, ncol=100)
+
+#free convection evaporative heat transfer coefficient
+hEFreePsy = matrix(array(), nrow=50, ncol=100)
+
+#associated evaporative "heat transfer" about the human body (free convection)
+QEvapFreePsy = matrix(array(), nrow=50, ncol=100)
+
+#solved mean radiant temperature required for thermal comfort taking free convection into account
+TMRTPsy = matrix(array(), nrow=50, ncol=100)
+
+#forced convection around the human body
+QConvForcedPsy = matrix(array(), nrow=50, ncol=100)
+
+#associated evaporative "heat transfer" about the human body (free convection)
+QEvapForcedPsy = matrix(array(), nrow=50, ncol=100)
+
+#solved mean radiant temperature required for thermal comfort taking forced convection into account
+TMRTForcedPsy = matrix(array(), nrow=50, ncol=100)
+
+#calculate mean radiant temperature when "dep" parameter is specified to solve for required air speed
+tempMRT = matrix(array(), nrow=50, ncol=100)
+
+#linearizing radiant heat transfer coefficient
+hRForcedV = matrix(array(), nrow=50, ncol=100)
+
+#linearized radiant heat transfer
+QRadForcedV = matrix(array(), nrow=50, ncol=100)
+
+#required air speed for thermal comfort using the 'dep' parameter to fix the mean radiant temperature
+vForcedV = matrix(array(), nrow=50, ncol=100)
+
+#2D relative humidity array dimension constant
+dim1 = nrow(RHPsy)
+
+#2D air temperature array dimension constant 
+dim2 = nrow(tempAir)
+
+#clausius-clapyron constants
+a=17.08
+b=234.1
+saturation = 1000*0.62198*exp(77.345+0.0057*(temperature+273.15)-7235/(temperature+273.15))/(101325*((temperature+273.15)^8.2)-exp(77.345+0.0057*(temperature+273.15)-7235/(temperature+273.15)))
+
+#for an arbitrary fixed comfort zone box calculation
+#Mapped everything from relative humidity to absolute humidity
+#BL = Bottom Left ...
+BL = 0.3*1000*0.62198*exp(77.345+0.0057*(21+273.15)-7235/(21+273.15))/(101325*((21+273.15)^8.2)-exp(77.345+0.0057*(21+273.15)-7235/(21+273.15)))
+TL = 0.6*1000*0.62198*exp(77.345+0.0057*(21+273.15)-7235/(21+273.15))/(101325*((21+273.15)^8.2)-exp(77.345+0.0057*(21+273.15)-7235/(21+273.15)))
+BR = 0.3*1000*0.62198*exp(77.345+0.0057*(27+273.15)-7235/(27+273.15))/(101325*((27+273.15)^8.2)-exp(77.345+0.0057*(27+273.15)-7235/(27+273.15)))
+TR = 0.6*1000*0.62198*exp(77.345+0.0057*(27+273.15)-7235/(27+273.15))/(101325*((27+273.15)^8.2)-exp(77.345+0.0057*(27+273.15)-7235/(27+273.15)))
+comfort = seq(from = 21, to = 26.9, by = 0.1)
+
+#forced convection heat transfer coefficient calculation
+hCForcedPsy = 10.1*(v^0.61)
+
+#forced convection evaporative heat transfer coefficient
+hEForcedPsy = hCForcedPsy*LR
+
+#ARRAY DECLARATIONS AND CALCULATIONS
+for (indexI in 1:50) {
+  tempAirInitVal = tempAirInit[indexI]
+  tempSkinVal = tempAirInitVal*.3812+22.406
+  PSatSkinPsyVal = (2.718^(77.3450+0.0057*(tempSkinVal+273.15)-7235/(tempSkinVal+273.15)))/(((tempSkinVal+273.15)^8.2))/1000
+  psySatValPre = 1000*0.62198*exp(77.345+0.0057*(tempAirInitVal+273.15)-7235/(tempAirInitVal+273.15))/(101325*((tempAirInitVal+273.15)^8.2)-exp(77.345+0.0057*(tempAirInitVal+273.15)-7235/(tempAirInitVal+273.15)))
+  PSatAirPsyValPre = (2.718^(77.3450+0.0057*(tempAirInitVal+273.15)-7235/(tempAirInitVal+273.15)))/(((tempAirInitVal+273.15)^8.2))/1000
+  hCFreePsyVal = 0.78*(abs(tempSkinVal-tempAirInitVal)^0.56)
+  QConvFreePsyVal = hCFreePsyVal*(tempSkinVal-tempAirInitVal)
+  hEFreePsyVal = hCFreePsyVal*LR
+  tempMRTVal = tempAirInitVal - dep
+  hRForcedVVal = 4*E*o*ArAd*((273.15+(tempSkinVal+tempMRTVal)/2)^3)
+  QRadForcedVVal = hRForcedVVal*E*(tempSkinVal-tempMRTVal)
   
-  cd = ifelse(1.2 < met & met< 2, round(clo * (0.6 + 0.4 / met), 3), clo)
-  cd
-} 
-
-# Estimates the running mean temperature
-runningMeanOutdoorTemperature <- function(tempArray, alpha=0.8, units="SI") {
-  if(tolower(units) == "ip"){
-    for(i in tempArray){
-      temp_array[ix] = units_converter(tdb=temp_array[ix])[1]
-    }
-  }
-  coeff = 0:(length(tempArray)-1)
-  trm = 0
-  
-  for(i in 1:length(coeff)){
-    coeff[i] = alpha^coeff[i]
-    trm = trm + (coeff[i] * tempArray[i])
-  }
-  trm = trm/sum(coeff)
-  if(tolower(units) == "ip"){
-    trm = units_converter(tmp=trm, fromUnits="si")[1]
-  }
-  round(trm,1)
+  for (indexJ in 1:100) {
+    RHPsyInitVal = RHPsyInit[indexJ]
+    PSatAirPsyVal = (2.718^(77.3450+0.0057*(tempAirInitVal+273.15)-7235/(tempAirInitVal+273.15)))/(((tempAirInitVal+273.15)^8.2))/1000*RHPsyInitVal
+    QEvapFreePsyVal = hEFreePsyVal*w*(PSatSkinPsyVal-PSatAirPsyVal)
+    QConvForcedPsyVal = hCForcedPsy*(tempSkinVal-tempAirInitVal)
+    QEvapForcedPsyVal = hEForcedPsy*w*(PSatSkinPsyVal-PSatAirPsyVal)
+    vForcedVal = (((MR-QRadForcedVVal)/(10.1*(LR*w*(PSatSkinPsyVal-PSatAirPsyVal)+(tempSkinVal-tempAirInitVal))))^(1/0.61))
+    
+    tempAir[indexI, indexJ] = tempAirInitVal
+    RHPsy[indexI, indexJ] = RHPsyInitVal
+    tempSkin[indexI, indexJ] = tempSkinVal
+    PSatSkinPsy[indexI, indexJ] = PSatSkinPsyVal
+    psySat[indexI, indexJ] = psySatValPre*RHPsyInitVal
+    PSatAirPsy[indexI, indexJ] = PSatAirPsyVal
+    hCFreePsy[indexI, indexJ] = hCFreePsyVal
+    QConvFreePsy[indexI, indexJ] = QConvFreePsyVal
+    hEFreePsy[indexI, indexJ] = hEFreePsyVal
+    QEvapFreePsy[indexI, indexJ] = QEvapFreePsyVal
+    TMRTPsy[indexI, indexJ] = ((((tempSkinVal+273.15)^4)-((MR-QEvapFreePsyVal-QConvFreePsyVal)/E/o/ArAd))^0.25)-273.15
+    QConvForcedPsy[indexI, indexJ] = QConvForcedPsyVal
+    QEvapForcedPsy[indexI, indexJ] = QEvapForcedPsyVal
+    TMRTForcedPsy[indexI, indexJ] = ((((tempSkinVal+273.15)^4)-((MR-QEvapForcedPsyVal-QConvForcedPsyVal)/E/o/ArAd))^0.25)-273.15
+    tempMRT[indexI, indexJ] = tempMRTVal
+    hRForcedV[indexI, indexJ] = hRForcedVVal
+    QRadForcedV[indexI, indexJ] = QRadForcedVVal
+    vForcedV[indexI, indexJ] = vForcedVal
+  } 
 }
 
-# Converts IP values to SI units
-unitsConverter <- function(from_units="ip", ...){
-  kwargs = list(...)
-  results = c()
-  
-  if(from_units == "ip"){
-    for(i in 1:length(kwargs)){
-      key = names(kwargs[i])
-      if(grepl("tmp", key) || key == "tr" || key == "tdb")  {
-        results <- c(results, round((kwargs[[i]] - 32) * 5 / 9,2))
-      }
-      if(key %in% c("v", "vr", "vel")) results <- c(results, round(kwargs[[i]] / 3.281, 2))
-      if(key == "area") results <- c(results, round(kwargs[[i]] / 10.764,2))
-      if(key == "pressure") results <- c(results, round(kwargs[[i]] * 101325,2))
-    }
-  }
-  else if(from_units == "si"){
-    for(i in 1:length(kwargs)){
-      key = names(kwargs[i])
-      if(grepl("tmp", key) || key == "tr" || key == "tdb")  {
-        results <- c(results, (kwargs[[i]] * 9 / 5) + 32)
-      }
-      if(key %in% c("v", "vr", "vel")) results <- c(results, kwargs[[i]] * 3.281)
-      if(key == "area") results <- c(results, kwargs[[i]] * 10.764)
-      if(key == "pressure") results <- c(results, kwargs[[i]] / 101325)
-    }
-  }
-  results
-}
+#set transparency
+alph = 0.6
+df = tbl_df(data.frame(temperature, saturation))
+df1 = tbl_df(data.frame(tempAir, psySat, vForcedV))
 
-# Calculates operative temperature in accordance with ISO 7726:1998 [5]_
-to <- function(tdb, tr, vel){
-  (tdb * sqrt(10 * vel) + tr) / (1 + sqrt(10 * vel))
-}
+abcd = ggplot(df, aes(x = temperature, y = saturation)) + ylim(0, 50) + 
+  geom_line() + 
+  geom_line(y = saturation*0.9, alpha = alph) + 
+  geom_line(y = saturation*0.8, alpha = alph) + 
+  geom_line(y = saturation*0.7, alpha = alph) + 
+  geom_line(y = saturation*0.6, alpha = alph) + 
+  geom_line(y = saturation*0.5, alpha = alph) + 
+  geom_line(y = saturation*0.4, alpha = alph) + 
+  geom_line(y = saturation*0.3, alpha = alph) + 
+  geom_line(y = saturation*0.2, alpha = alph) + 
+  geom_line(y = saturation*0.1, alpha = alph)
 
-# Calculates air enthalpy
-enthalpy <- function(tdb, hr){
-  hDryAir = cpAir * tdb
-  hSatVap = hFg + cpVapour * tdb
-  h = hDryAir + hr * hSatVap
-  round(h, 2)
-}
-
-# Calculates vapour pressure of water at different temperatures
-pSat <- function(tdb){
-  taK = tdb + cToK
-  c1 = -5674.5359
-  c2 = 6.3925247
-  c3 = -0.9677843 * 10^-2
-  c4 = 0.62215701 * 10^-6
-  c5 = 0.20747825 * 10^-8
-  c6 = -0.9484024 * 10^-12
-  c7 = 4.1635019
-  c8 = -5800.2206
-  c9 = 1.3914993
-  c10 = -0.048640239
-  c11 = 0.41764768 * 10^-4
-  c12 = -0.14452093 * 10^-7
-  c13 = 6.5459673
-  
-  if(taK < cToK){
-    round(exp(
-      c1 / taK
-      + c2
-      + taK * (c3 + taK * (c4 + taK * (c5 + c6 * taK)))
-      + c7 * log(taK)
-    ),1)
-  }
-  else{
-    round(exp(
-      c8 / taK
-      + c9
-      + taK * (c10 + taK * (c11 + taK * c12))
-      + c13 * log(taK)
-    ),1)
-  }
-}
-
-# Calculates psychrometric values of air based on dry bulb air temperature and 
-# relative humidity
-psyTaRh <- function(tdb, rh, patm=101325){
-  psat = pSat(tdb)
-  pvap = rh / 100 * psat
-  hr = 0.62198 * pvap / (patm - pvap)
-  tdp = tDp(tdb, rh)
-  twb = tWb(tdb, rh)
-  h = enthalpy(tdb, hr)
-  
-  list("pSat"= psat, "pVap"= pvap, "hr"= hr, "tWb"= twb, "tDp"= tdp, "h"= h)
-}
-
-# Calculates the wet-bulb temperature using the Stull equation [6]_
-tWb <- function(tdb, rh){
-  round(
-    tdb * atan(0.151977 * (rh + 8.313659) ** (1 / 2))
-    + atan(tdb + rh)
-    - atan(rh - 1.676331)
-    + 0.00391838 * rh ** (3 / 2) * atan(0.023101 * rh)
-    - 4.686035,
-    1)
-}
-
-# Calculates the dew point temperature
-tDp <- function(tdb, rh){
-  c = 257.14
-  b = 18.678
-  a = 6.1121
-  d = 234.5
-  gammaM = log(rh / 100 * exp((b - tdb / d) * (tdb / (c + tdb))))
-  round(c * gammaM / (b - gammaM), 1)
-}
-
-# Converts globe temperature reading into mean radiant temperature in accordance
-# with ISO 7726:1998 [5]_
-tMrt <- function(tg, tdb, v, d=0.15, emissivity=0.9){
-  tg = tg + cToK
-  tdb = tdb + cToK
-  
-  # calculate heat transfer coefficient
-  hn = 1.4 * (abs(tg - tdb) / d) ^ 0.25  # natural convection
-  hf = 6.3 * v ^ 0.6 / d ^ 0.4  # forced convection
-  
-  # get the biggest between the tow coefficients
-  h = max(hf, hn)
-  
-  round((tg ^ 4 + h * (tg - tdb) / (emissivity * (5.67 * 10 ^ -8))) ^ 0.25 - cToK,1)
+if (dep != 0){
+  levelsContour = seq(0.1, 2, length.out= 150)
+  CS3 = abcd +
+    stat_contour_filled(mapping = aes(x = temperature, y = saturation, z = temperature), binwidth=10)
 }
