0258586337ebb3948b7ccb07cca89a31e5883c79
Shaomi Rahman <shaomi.rahman@rwth-aachen.de>
2021-03-22 12:29:10 +0100

    updated help functions

--- R/helpJOS.R
+++ R/helpJOS.R
@@ -67,6 +67,116 @@ options <- function(){
 ##############################################
 
 run <- function(dtime=60, passive=False, output=True){
+  tcr <- Tcr()
+  tsk <- Tsk()
+  
+  # Convective and radiative heat transfer coefficient [W/K.m2]
+  posture <- "standing"
+  va <- rep(0.1,17)
+  ta <- rep(28.8,17)
+  hc <- fixedHc(convCoef(posture, va, ta, tsk), va)
+  hr <- fixedHr(radCoef(posture))
+  
+  # Operarive temp. [oC], heat and evaporative heat resistance [K/W], [kPa/W]
+  tr <- rep(28.8,17)
+  clo <- rep(0, 17)
+  iclo <- rep(0.45, 17)
+  to <- operativeTemp(ta, tr, hc, hr)
+  rT <- dryR(hc, hr, clo)
+  rEt <- wetR(hc, clo, iclo) 
+  
+  # Thermoregulation
+  
+  # Setpoint temperature of thermoregulation
+  if(passive){
+    setptCr <- tcr
+    setptSk <- tsk
+  }
+  else{
+    setptCr <- rep(37, 17)
+    setptSk <- rep(34, 17)
+  }
+  
+  # Difference between setpoint and body temperatures
+  errCr <- tcr - setptCr
+  errSk <- tsk - setptSk
+  
+  # Skinwettedness [-], Esk, Emax, Esw [W]
+  rh <- rep(50, 17)
+  height <- 1.72
+  weight <- 74.43
+  bsaEquation <- "dubois"
+  age <- 20
+  wet <- evaporation(errCr, errSk, tsk, ta, rh, rEt, height, weight, bsaEquation,
+                     age)
+  eSk <- wet
+  eMax <- wet
+  eSweat <- wet
+  
+  # Skin blood flow, basal skin blood flow [L/h]
+  ci <- 2.59
+  bfSk <- skinBloodflow(errCr, errSk, height, weight, bsaEquation, age, ci)
+  
+  # Hand, Foot AVA blood flow [L/h]
+  options <- options()
+  bfAvaHand <- avaBloodflow(errCr, errSk, height, weight, bsaEquation, age, ci)
+  bfAvaFoot <- bfAvaHand
+  if(options[["ava_zero"]] & passive){
+    bfAvaHand = 0
+    bfAvaFoot = 0
+  }
+  
+  # Thermogenesis by shivering [W]
+  sex <- "male"
+  mshiv <- shivering(errCr, errSk, tcr, tsk, height, weight, bsaEquation, age, 
+                     sex, dtime, options)
+  
+  # Thermogenesis by non-shivering [W]
+  if(options[["nonshivering_thermogenesis"]]){
+    mnst <- nonshivering(errCr, errSk, height, weight, bsaEquation, age, 
+                        options[["cold_acclimated"]], options[["bat_positive"]])
+  }
+  else{
+    mnst <- rep(0, 17)
+  }
+  
+  # Thermogenesis
+  # Basal thermogenesis [W]
+  mbase <- localMbase(height, weight, age, sex, bsaEquation)
+  mbaseAll <- 0
+  for(m in mbase){
+    mbaseAll <- mbaseAll + sum(m) 
+  }
+  
+  # Thermogenesis by work [W]
+  par <- 1.25
+  mwork <- localMwork(mbaseAll, par)
+  
+  # Sum of thermogenesis in core, muscle, fat, skin [W]
+  sumMRes <- sumM(mbase, mwork, mshiv, mnst)
+  qcr <- sumMRes$qcr
+  qms <- sumMRes$qms
+  qfat <- sumMRes$qfat
+  qsk <- sumMRes$qsk
+  
+  qall <-  sum(qcr) + sum(qms) + sum(qfat) + sum(qsk)
+  
+  # Other
+  # Blood flow in core, muscle, fat [L/h]
+  crmsfatBloodflowRes <- crmsfatBloodflow(mwork, mshiv, height, weight, 
+                                          bsaEquation, age, ci)
+  bfCr <- crmsfatBloodflowRes$bfCr
+  bfMs <- crmsfatBloodflowRes$bfMs
+  bfFat <- crmsfatBloodflowRes$bfFat
+  
+  # Heat loss by respiratory
+  pA <- rep(NA, 17)
+  antoineRes <- antoine(ta)
+  for(i in 1:length(pa)){
+    pA[i] <- antoine[i] * rh[i] /100
+  }
+  
+  
 }
 
 ##############################################
@@ -122,7 +232,8 @@ calcIndexOrder <- function(){
 
 calcBodyTemp <- function(){
   numNodes <- calcIndexOrder()[[2]]
-  bodyTemp = rep.int(36, 85)
+  bodyTemp <- rep.int(36, 85)
+  bodyTemp
 }
 
 ##############################################
@@ -135,4 +246,95 @@ calcIndexByLayer <- function(layer){
     }
   }
     
+}
+
+##############################################
+
+Tcr <- function(){
+  10
+}
+
+##############################################
+
+Tsk <- function(){
+  10
+}
+
+##############################################
+
+fixedHc <- function(posture, va, ta, tsk){
+  
+}
+
+##############################################
+
+fixedHr <- function(hr){
+  
+}
+
+##############################################
+
+convCoef <- function(hc, va) {
+  
+}
+
+##############################################
+
+radCoef <- function(posture){
+  
+}
+
+##############################################
+
+operativeTemp <- function(ta, tr, hc, hr){
+  
+}
+
+##############################################
+
+dryR <- function(hc, hr, clo){
+  
+}
+
+##############################################
+
+wetR <- function(hc, clo, iclo = 0.45, lewisRate = 16.5){
+  
+}
+
+##############################################
+
+options <- function(){
+  list("nonshivering_thermogenesis" = TRUE, 
+       "cold_acclimated" = FALSE, 
+       "shivering_threshold" = FALSE, 
+       "limit_dshiv/dt" = FALSE, 
+       "bat_positive" = FALSE, 
+       "ava_zero" = FALSE,
+       "shivering" = FALSE)
+}
+
+##############################################
+
+localMbase <- function(){
+  return(mapply(c, c(1,2), c(2,3), SIMPLIFY=FALSE))
+}
+
+##############################################
+
+sumM <- function(mbase, mwork, mshiv, mnst){
+ list(qcr = c(1,2), qms = c(1,2), qfat = c(1,2), qsk = c(1,2)) 
+}
+
+##############################################
+
+crmsfatBloodflow <- function(mwork, mshiv, height=1.72, weight=74.43, 
+                             equation="dubois", age=20, ci=2.59){
+  list(bfCr = c(1,2), bfMs = c(1,2), bfFat = c(1,2)) 
+}
+
+##############################################
+
+antoine <- function(ta){
+  rep(1, 17)
 }
