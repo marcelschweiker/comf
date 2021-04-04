5b3bbddfd68afd8d9699104e980b71af595978bd
ShoaibSar <73479375+ShoaibSar@users.noreply.github.com>
2021-04-04 21:28:28 +0200

    Update helpJOS.R

--- R/helpJOS.R
+++ R/helpJOS.R
@@ -12,7 +12,15 @@ calcFujimoto <- function(height,weight){
 calcKurazumi <- function(height,weight){
   return(0.2440 * (height ^ 0.693) * (weight ^ 0.383))
 }
-
+# Antoine equation [kPa]
+calcAntoine <- function(x){
+  return(exp(16.6536-(4030.183/(x+235))))
+}
+# Tetens equation [kPa]
+calcTetens <- function(x){
+  return(0.61078*10^(7.5*x/(x+237.3)))
+}
+calcTetens(5)
 ###############################################
 
 BSAst <- function(){
@@ -29,3 +37,73 @@ calcWeightrate <- function(weight=74.43){
 }
 
 ##############################################
+
+calcfixedHC <- function(hc, va){
+mean_hc <- weighted.mean(hc, weights=BSAst())
+mean_va <- weighted.mean(va, weights=BSAst())
+mean_hc_whole <- max(3, 8.600001*(mean_va**0.53))
+fixed_hc <- hc * mean_hc_whole/mean_hc
+return(fixed_hc)
+}
+
+##############################################
+
+calcfixedHR <- function(hr){
+mean_hr <- weighted.mean(hr, weights=BSAst())
+fixed_hr <- hr * 4.7/mean_hr
+return(fixed_hr)
+}
+
+##############################################
+
+calcoperativeTEMP <- function(ta, tr, hc, hr){
+otemp <- (hc*ta + hr*tr) / (hc + hr)
+return(otemp)
+}
+
+##############################################
+
+calcCLOarea <- function(clo){
+if (clo<0.5){
+  fcl <- clo*0.2+1
+}
+else { fcl <- clo*0.1+1.05
+}
+return(fcl)
+}
+
+##############################################
+
+calcdryR <- function(hc, hr, clo){
+
+  fcl <- calcCLOarea(clo)
+  ra <- 1/(hc+hr)
+  rcl <- 0.155*clo
+  rt <- ra/fcl + rcl
+  return(rt)
+}
+
+#############################################
+
+calcwetR <- function(hc, clo, iclo=0.45, lewis_rate=16.5){
+
+fcl <- calcCLOarea(clo)
+rcl <- 0.155 * clo
+rea <- 1 / (lewis_rate * hc)
+recl <- rcl / (lewis_rate * iclo)
+ret <- rea / fcl + recl
+return(ret)
+}
+
+#############################################
+#Calculate local metabolic rate by work [W]
+calcMworklocal <- function(bmr, par){
+mwork_all <- (par-1) * bmr
+mwf <- c(
+  0, 0, 0.091, 0.08, 0.129,
+  0.0262, 0.0139, 0.005, 0.0262, 0.0139, 0.005,
+  0.2010, 0.0990, 0.005, 0.2010, 0.0990, 0.005)
+mwork <- mwork_all * mwf
+return(mwork)
+}
+#############################################
