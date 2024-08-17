ed4cc964eff07428feb11727f362e6d797261b31
Twin Gan <twin.c.gan@gmail.com>
2024-08-17 15:03:01 +1000

    add test  aPMV

---
+++ tests/testthat/test_calcaPMV.R
@@ -0,0 +1,13 @@
+library(testthat)
+library(comf)
+# Define the test
+test_that("Test calcaPMV function outputs", {
+
+  actual_results <- calcaPMV(c(24, 30), 30, vel=0.22, rh=50, met=1.4, clo=0.5, apCoeff=0.293)
+
+  # Define the expected results
+  expected_results <- c(0.48, 1.09)
+
+  # Test if the actual results match the expected results
+  expect_equal(actual_results, expected_results)
+})
