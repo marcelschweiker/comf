947bcd54c8668617248388b2805753bb4d5cd9f9
Wangjiayi1115 <jwan0698@uni.sydney.edu.au>
2024-08-21 20:41:29 +1000

    add new changes

--- R/calcAD.R
+++ R/calcAD.R
@@ -46,5 +46,6 @@ calcAD <- function(ta,tr,vel,rh,clo,met,vAnkle){
        warning('Velocity(vel) should be less than or equal to 0.2')
 
      }
+    data.frame(round(tsv,3), ppdval, acceptability)
 
 }
