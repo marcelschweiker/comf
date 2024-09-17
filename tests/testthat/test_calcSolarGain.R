7257053de7edf3e26f54600a293ad67562ec4d03
Twin Gan <twin.c.gan@gmail.com>
2024-09-18 00:22:40 +1000

    fix solargain variable names because of json changed

--- tests/testthat/test_calcSolarGain.R
+++ tests/testthat/test_calcSolarGain.R
@@ -5,18 +5,17 @@ test_that("test calcSolarGain", {
   reference_tables <- retrieve_data(url_config$test_solar_gain_url)
   tolerance <- reference_tables$tolerance
   data <- reference_tables$data
-
   for (i in seq_len(nrow(data))) {
     inputs <- data[i, "inputs"]
     outputs <- data[i, "outputs"]
     sg <- calcSolarGain(
-      solAlt = inputs$alt,
+      solAlt = inputs$sol_altitude,
       solAzi = inputs$sharp,
-      solRadDir = inputs$I_dir,
-      solTrans = inputs$t_sol,
+      solRadDir = inputs$sol_radiation_dir,
+      solTrans = inputs$sol_transmittance,
       fSvv = inputs$f_svv,
       fBes = inputs$f_bes,
-      asw = inputs$asa,
+      asw = inputs$asw,
       posture = inputs$posture
     )
     expect_true(abs(sg[1] - outputs$erf) < tolerance$erf,
