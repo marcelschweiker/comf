b19be1ae20f10f9ebdf883fd932648e83dad2a02
number9527-12 <lizheyu2000@gmail.com>
2024-10-08 14:43:27 +1100

    Add test url

--- tests/config.R
+++ tests/config.R
@@ -8,10 +8,11 @@ url_config <- list(
   test_ankle_draft_url = paste0(unit_test_data_prefix, "ts_ankle_draft.json"),
   test_vtg_url = paste0(unit_test_data_prefix, "ts_vertical_tmp_grad_ppd.json"),
   test_humidex_url = paste0(unit_test_data_prefix, "ts_humidex.json"),
-  test_pmv_ppd_url = paste0(unit_test_data_prefix, "ts_pmv_pdd.json"),
+  test_pmv_ppd_url = paste0(unit_test_data_prefix, "ts_pmv_ppd.json"),
   test_set_url = paste0(unit_test_data_prefix, "ts_set.json"),
-  test_e_pmv_url = paste0(unit_test_data_prefix,"ts_e_pmv.json"),
-  test_utci_url = paste0(unit_test_data_prefix,"ts_utci.json"),
-  test_athb_url = paste0(unit_test_data_prefix,"ts_athb.json"),
-  test_two_node_url = paste0(unit_test_data_prefix,"ts_two_nodes.json")
+  test_e_pmv_url = paste0(unit_test_data_prefix, "ts_e_pmv.json"),
+  test_utci_url = paste0(unit_test_data_prefix, "ts_utci.json"),
+  test_athb_url = paste0(unit_test_data_prefix, "ts_athb.json"),
+  test_two_node_url = paste0(unit_test_data_prefix, "ts_two_nodes.json"),
+  test_ce_url = paste0(unit_test_data_prefix, "ts_cooling_effect.json")
 )
