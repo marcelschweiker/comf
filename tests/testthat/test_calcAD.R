fb47dc54d5bff8566553b1990f5d711ec4c73d20
Jiayi Wang <jwan0698@uni.sydney.edu.au>
2024-10-09 18:01:14 +1100

    fix error for AD

--- tests/testthat/test_calcAD.R
+++ tests/testthat/test_calcAD.R
@@ -11,25 +11,20 @@ test_that("test calcAD", {
     result <- calcAD(
       ta = inputs$tdb,
       tr = inputs$tr,
-      vel = inputs$v,
+      vel = inputs$vr,
       rh = inputs$rh,
       met = inputs$met,
       clo = inputs$clo,
       vAnkle = inputs$v_ankle
     )
     expect_true(
-      abs(result$Ankle_draft_ppd - outputs$Ankle_draft_ppd)
-      < tolerance$Ankle_draft_ppd,
-      info = paste("Failed at data row", i, ":Ankle_draft_ppd tolerance check.")
+      abs(result$ppdAd - outputs$PPD_ad)
+      < tolerance$PPD_ad,
+      info = paste("Failed at data row", i, ":Ankle_draft_ppd tolerance check.", "|result: ", result$ppdAd, "expect_outputs: ", outputs)
     )
   }
   # Test for error with invalid input
-  expect_error(calcAD(ta = 25, tr = 25, vel = 0.3, rh = 50, met = 1.2,
+  expect_warning(calcAD(ta = 25, tr = 25, vel = 0.3, rh = 50, met = 1.2,
     clo = 0.5, vAnkle = 7
   ))
-  # Test that 'units' parameter is not supported
-  expect_error(calcAD(ta = 25, tr = 25, vel = 0.3, rh = 50, met = 1.2,
-      clo = 0.5, vAnkle = 0.3, units = "SI"
-    ), "unused argument"
-  )
 })
