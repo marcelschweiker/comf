f1cba70fb8376eb8f11e6272f697b8ded31578bc
LeylaYiqingZhang <yzha0567@uni.sydney.edu.au>
2024-10-09 17:51:03 +1100

    delete comments

--- R/calcCloTout.R
+++ R/calcCloTout.R
@@ -17,12 +17,11 @@
 #' @export
 
 calcCloTout <- function(tout, units = "SI") {
-  # Check if tout is numeric or a list of numeric values
+
   if (!is.numeric(tout) && !is.list(tout)) {
     stop("tout must be numeric or a list of numeric values.")
   }
-  
-  # Convert list to numeric vector if necessary
+
   if (is.list(tout)) {
     tout <- unlist(tout)
     if (!is.numeric(tout)) {
@@ -34,22 +33,18 @@ calcCloTout <- function(tout, units = "SI") {
     units <- "SI"
   }
 
-  # Validate the units
   valid_units <- c("SI", "IP")
   if (!(toupper(units) %in% valid_units)) {
     stop(paste("Invalid unit:", units, ". Supported units are", toString(valid_units), "."))
   }
 
-  # Convert units if necessary (from IP to SI)
   if (tolower(units) == "ip") {
-    tout <- (tout - 32) * 5/9  # Convert Fahrenheit to Celsius
+    tout <- (tout - 32) * 5/9
   }
 
-  # Calculate clo based on tout
   clo <- ifelse(tout < 26, 10^(-0.1635 - 0.0066 * tout), 0.46)
   clo <- ifelse(tout < 5, 0.818 - 0.0364 * tout, clo)
   clo <- ifelse(tout < -5, 1, clo)
 
-  # Round to 2 decimal places
   return(round(clo, 2))
 }
