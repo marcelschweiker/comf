3b94af680f2fc2a311a4fafbc1c8f87f8ae116d3
marcelschweiker <18235473+marcelschweiker@users.noreply.github.com>
2024-10-21 21:05:41 +0200

    Update calcATHBstandard.R
    Added function to use observed clo-values instead of using those calculated based on Trm

--- R/calcATHBstandard.R
+++ R/calcATHBstandard.R
@@ -4,7 +4,7 @@
 #' @description \code{calcATHBstandard} calculates the PMV based on adaptive thermal heat balance framework 
 #' @description based on the newest version (2022)
 #' 
-#' @usage calcATHBstandard(trm, ta, tr, vel, rh, met)
+#' @usage calcATHBstandard(trm, ta, tr, vel, rh, met, clo = 0.5, cloAdapt = FALSE)
 #'
 #' @param trm - Running mean outdoor temperature in [degree C]
 #' @param ta - a numeric value presenting air temperature in [degree C]
@@ -12,6 +12,9 @@
 #' @param vel - a numeric value presenting air velocity in [m/s]
 #' @param rh - a numeric value presenting relative humidity [\%]
 #' @param met - a numeric value presenting metabolic rate in [met]
+#' @param clo - a numeric value presenting clothing insulation level in [clo] 
+#' @param cloAdapt - a boolean value with FALSE as default that adapted clothing 
+#' @param insulation level calculated based on Trm. TRUE meaning that observed clo-value is required as input
 #'
 #' @return \code{calcATHBstandard} PMV value adapted through the ATHB approach with standard model
 #'
@@ -26,14 +29,19 @@
 #'
 #' @examples calcATHBstandard(20, 25, 25, .1, 50, 1.1)
 
-calcATHBstandard <- function(trm, ta, tr, vel, rh, met){
+calcATHBstandard <- function(trm, ta, tr, vel, rh, met, clo = 0.5, cloAdapt = FALSE){
   
   # metabolic rate through physiological adaptation
   metAdpt <- met - (0.234 * trm) / 58.2
   
   # adapted clothing insulation level through behavioural adaptation
-  cloAdpt <- 10 ^ (-0.1716848 - 0.0004853 * trm + 0.0817623 * metAdpt - 0.0052730 * trm * metAdpt)
-
+  if(cloAdapt = FALSE){
+	cloAdpt <- 10 ^ (-0.1716848 - 0.0004853 * trm + 0.0817623 * metAdpt - 0.0052730 * trm * metAdpt)
+  } else if(cloAdapt = TRUE){
+	cloAdpt <- clo
+  } else {
+	stop("error: cloAdapt can only be FALSE or TRUE")
+  }
   # adapted thermal load according to Fanger’s PMV mode
   LAdpt <- calcPMVPPD(ta, tr, vel, rh, cloAdpt, metAdpt, getLoad = TRUE)$Lraw
   
