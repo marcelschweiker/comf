ce9dcf88da137aaf042a276dd58e0f339d6f16c2
LeylaYiqingZhang <yzha0567@uni.sydney.edu.au>
2024-10-25 17:23:58 +1100

    final version of function and test for heatindex

--- tests/testthat/test_calcHeatIndex.R
+++ tests/testthat/test_calcHeatIndex.R
@@ -1,26 +1,25 @@
 test_that("test calcHeatIndex", {
-  # Load necessary configurations and utilities
   source("../config.R")
   source("../utils-test-tool.R")
   
-  # Retrieve test data from the URL defined in the config
   reference_tables <- retrieve_data(url_config$test_heat_index_url)
   tolerance <- reference_tables$tolerance
   data <- reference_tables$data
-
-  # Loop through the rows of test data and check the calcHeatIndex function
+  
   for (i in seq_len(nrow(data))) {
     inputs <- data[i, "inputs"]
     outputs <- data[i, "outputs"]
 
+    units <- ifelse(is.null(inputs$units), "SI", inputs$units)
+    
     result <- calcHeatIndex(
-      temp = inputs$temp,
-      rh = inputs$rh,
-      units = inputs$units
+      tdb = inputs$tdb[[1]],
+      rh = inputs$rh[[1]],
+      units = units
     )
 
-    # Assert that the calculated result is within the acceptable tolerance
-    expect_true(abs(result - outputs$result) < tolerance$result,
+    expect_true(abs(result - outputs$hi) < tolerance$hi,
       info = paste("Failed at data row", i, ": Heat Index tolerance check.")
     )
   }
+})
