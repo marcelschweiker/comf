ad358ff3160e6027113dcb2160361a1a9660b4c3
Twin Gan <twin.c.gan@gmail.com>
2024-11-15 14:31:27 +1100

    solve conflict

--- tests/testthat/test-calctAdapt15251.R
+++ tests/testthat/test-calctAdapt15251.R
@@ -1,30 +1,25 @@
 test_that("test calctAdapt15251", {
+
   reference_tables <- retrieve_data(url_config$test_adaptive_en_url)
   tolerance <- reference_tables$tolerance
   data <- reference_tables$data
-
+  
   for (i in seq_len(nrow(data))) {
     inputs <- data[i, "inputs"]
     outputs <- data[i, "outputs"]
+    input.list <- if (is.null(inputs$t_running_mean[[1]])) list(NA) else as.list(inputs$t_running_mean[[1]])
+    output.list <- if (is.null(outputs$tmp_cmf[[1]])) list(NA) else as.list(outputs$tmp_cmf[[1]])
+    result <- calctAdapt15251(input.list)
 
-    if (isFALSE(data$execute_in_R[i])) {
-      print(paste("Skipping test case", i, "due to 'execute_in_R' being FALSE"))
-      next
-    }
-
-    expected_output <- outputs$tmp_cmf[[1]]
-
-    if (is.null(expected_output)) {
-      next
-    }
-
-    result <- calctAdapt15251(inputs$t_running_mean[[1]])
-    expect_true(abs(result$tAdapt15251 - expected_output) <= tolerance,
-      info = paste(
-        "Failed at data row", i, ": Adapt15251 tolerance check. inputs:",
-        inputs$t_running_mean[[1]], "outputs: ", outputs$tAdapt15251,
-        "real outputs:", result$tAdapt15251
+    expect_true(
+        all(mapply(function(x, y) {
+            (is.na(x) && is.na(y)) || (!is.na(x) && !is.na(y) && abs(x - y) < tolerance[[1]])  
+          },
+          result, output.list)),
+        info = paste(
+          "Failed at data row", i, ": Adapt15251 tolerance check. inputs:",
+          inputs$trm, "outputs:", outputs$tAdapt15251
+        )
       )
-    )
   }
 })
--- tests/testthat/test-calctAdaptASHRAE.R
+++ tests/testthat/test-calctAdaptASHRAE.R
@@ -1,31 +1,26 @@
 test_that("test calctAdaptASHRAE", {
+  
   reference_tables <- retrieve_data(url_config$test_adaptive_ashrae_url)
   tolerance <- reference_tables$tolerance
   data <- reference_tables$data
-
+ 
   for (i in seq_len(nrow(data))) {
     inputs <- data[i, "inputs"]
     outputs <- data[i, "outputs"]
-    if (isFALSE(data$execute_in_R[i])) {
-      print(paste("Skipping test case", i, "due to 'execute_in_R' being FALSE"))
-      next
-    }
     tmmo <- if (is.null(inputs$t_running_mean[[1]])) list(NA) else as.list(inputs$t_running_mean[[1]])
     units <- if ("units" %in% names(inputs)) inputs$units[[1]] else "si"
     output.list <- if (is.null(outputs$tmp_cmf[[1]])) list(NA) else as.list(outputs$tmp_cmf[[1]])
     result <- calctAdaptASHRAE(tmmo, units = units)
-
+    
     expect_true(
-      all(mapply(
-        function(x, y) {
-          (is.na(x) && is.na(y)) || (!is.na(x) && !is.na(y) && abs(x - y) < tolerance[[1]])
-        },
-        result, output.list
-      )),
-      info = paste(
-        "Failed at data row", i, ": AdaptASHRAE tolerance check. inputs:",
-        inputs$tmmo, "outputs:", outputs$tAdaptASHRAE
+      all(mapply(function(x, y) {
+        (is.na(x) && is.na(y)) || (!is.na(x) && !is.na(y) && abs(x - y) < tolerance[[1]])  
+          },
+          result, output.list)),
+        info = paste(
+          "Failed at data row", i, ": AdaptASHRAE tolerance check. inputs:",
+          inputs$tmmo, "outputs:", outputs$tAdaptASHRAE
+        )
       )
-    )
   }
 })
