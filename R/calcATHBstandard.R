0ae6acb4e2375c055541365a96863caa09368fd8
marcelschweiker <18235473+marcelschweiker@users.noreply.github.com>
2024-12-21 20:32:19 +0100

    Update calcATHBstandard.R
    Meaning of cloAdapt reverted to be more logical.
    Details and explanation added to documentation.

--- R/calcATHBstandard.R
+++ R/calcATHBstandard.R
@@ -4,7 +4,7 @@
 #' @description \code{calcATHBstandard} calculates the PMV based on adaptive thermal heat balance framework 
 #' @description based on the newest version (2022)
 #' 
-#' @usage calcATHBstandard(trm, ta, tr, vel, rh, met, clo = 0.5, cloAdapt = FALSE)
+#' @usage calcATHBstandard(trm, ta, tr, vel, rh, met, clo = 0.5, cloAdapt = TRUE)
 #'
 #' @param trm - Running mean outdoor temperature in [degree C]
 #' @param ta - a numeric value presenting air temperature in [degree C]
@@ -13,10 +13,13 @@
 #' @param rh - a numeric value presenting relative humidity [\%]
 #' @param met - a numeric value presenting metabolic rate in [met]
 #' @param clo - a numeric value presenting clothing insulation level in [clo] 
-#' @param cloAdapt - a boolean value with FALSE as default that adapted clothing 
+#' @param cloAdapt - a boolean value with TRUE as default that adapted clothing is used
 #' @param insulation level calculated based on Trm. TRUE meaning that observed clo-value is required as input
 #'
 #' @return \code{calcATHBstandard} PMV value adapted through the ATHB approach with standard model
+#'
+#' @details By default (with cloAdapt = TRUE), the clothing level will be calculated based on trm and any input to clo ignored.
+#' @details If users want to use actual clothing insulation level, set cloAdapt to FALSE and define clo.
 #'
 #' @references 
 #' Schweiker & Wagner (2015) <doi:10.1016/j.buildenv.2015.08.018>
@@ -29,15 +32,15 @@
 #'
 #' @examples calcATHBstandard(20, 25, 25, .1, 50, 1.1)
 
-calcATHBstandard <- function(trm, ta, tr, vel, rh, met, clo = 0.5, cloAdapt = FALSE){
+calcATHBstandard <- function(trm, ta, tr, vel, rh, met, clo = 0.5, cloAdapt = TRUE){
   
   # metabolic rate through physiological adaptation
   metAdpt <- met - (0.234 * trm) / 58.2
   
   # adapted clothing insulation level through behavioural adaptation
-  if(cloAdapt = FALSE){
+  if(cloAdapt = TRUE){
 	cloAdpt <- 10 ^ (-0.1716848 - 0.0004853 * trm + 0.0817623 * metAdpt - 0.0052730 * trm * metAdpt)
-  } else if(cloAdapt = TRUE){
+  } else if(cloAdapt = FALSE){
 	cloAdpt <- clo
   } else {
 	stop("error: cloAdapt can only be FALSE or TRUE")
