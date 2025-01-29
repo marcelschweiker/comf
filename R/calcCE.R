82e5175decd9d0bd59691fc0f0ed9ea621228ec5
federico tartarini <federicotartarini@gmail.com>
2025-01-29 14:36:36 +1100

    fix: cooling effect removed rounding

--- R/calcCE.R
+++ R/calcCE.R
@@ -19,24 +19,21 @@
 #' @export
 
 
-calcCE <- function(ta, tr, vel, rh, clo = .5, met = 1, wme = 0) {
-  if (vel <= 0.2) {
+calcCE <- function(ta, tr, vel, rh, clo, met, wme = 0) {
+  still_air_threshold <- 0.1
+  if (vel <= still_air_threshold) {
     ce <- 0
     return(ce)
   }
 
-
-  still_air_threshold <- 0.1
   initial_set_tmp <- calcSET(ta = ta, tr = tr, vel = vel, rh = rh, clo = clo, met = met, wme = wme)
-  initial_set_tmp <- round(initial_set_tmp, 1)
   f <- function(x) {
     change <- calcSET(ta - x, tr - x, vel = still_air_threshold, rh = rh, clo = clo, met = met, wme = wme)
-    change <- round(change, 1)
     return(change - initial_set_tmp)
   }
   out <- tryCatch(
     {
-      ce <- bisect(f, 0, 15)
+      ce <- bisect(f, 0, 40)
       return(ce)
     },
     error = function(cond) {
